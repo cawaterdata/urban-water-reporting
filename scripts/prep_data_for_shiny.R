00a80b07b901342e7d495256eed6e71eb1c1e035
ErinCain <ecain@flowwest.com>
2022-03-29 09:30:10 -0700

    Creates markdowns for moulton niguel

--- scripts/prep_data_for_shiny.R
+++ scripts/prep_data_for_shiny.R
@@ -9,6 +9,9 @@ napa_d <- read_rds(file = "data/napa_total_water_demand_af_deltas.rds") %>%
 santa_fe_d <- read_rds(file = "data/santa_fe_total_water_demand_af_deltas.rds") %>% 
   mutate(metric = "Total Water Demand") %>% glimpse()
 
+moulton_niguel_d <- read_rds(file = "data/moulton_niguel_total_water_demand_af_deltas.rds") %>% 
+  mutate(metric = "Total Water Demand") %>% glimpse()
+
 # Water Supply 
 napa_s <- read_rds(file = "data/napa_total_water_supply_af_deltas.rds") %>% 
   mutate(metric = "Total Water Supply") %>% glimpse()
@@ -16,7 +19,10 @@ napa_s <- read_rds(file = "data/napa_total_water_supply_af_deltas.rds") %>%
 santa_fe_s <- read_rds(file = "data/santa_fe_total_water_supply_af_deltas.rds") %>% 
   mutate(metric = "Total Water Supply") %>% glimpse()
 
-urban_water_reporting_data <- bind_rows(napa_d, santa_fe_d, napa_s, santa_fe_s) %>% glimpse()
+moulton_niguel_s <- read_rds(file = "data/moulton_niguel_total_water_supply_af_deltas.rds") %>% 
+  mutate(metric = "Total Water Supply") %>% glimpse()
+
+urban_water_reporting_data <- bind_rows(napa_d, santa_fe_d, moulton_niguel_d, napa_s, santa_fe_s, moulton_niguel_s) %>% glimpse()
 
 write_rds(urban_water_reporting_data, "data/urban_water_reporting_data.rds")
 
@@ -34,14 +40,26 @@ write_rds(supply_and_demand_volume_af, "data/supply_and_demand_volume_af.rds")
 
 # supply and demand with categories 
 
-#TODO add supply 
 santa_fe_d_with_subcategories <- read_rds("data/santa_fe_d_with_subcategories.rds") %>% 
   mutate(parent_metric = "Total Water Demand")
 
 napa_d_with_subcategoreies <- read_rds("data/napa_d_with_subcategories.rds") %>% 
   mutate(parent_metric = "Total Water Demand")
 
-supply_and_demand_volume_af_with_subcategories <- bind_rows(santa_fe_d_with_subcategories, napa_d_with_subcategoreies) %>% glimpse()
+moulton_niguel_d_with_subcategoreies <- read_rds("data/moulton_niguel_d_with_subcategories.rds") %>% 
+  mutate(parent_metric = "Total Water Demand")
+
+santa_fe_s_with_subcategories <- read_rds("data/santa_fe_s_with_subcategories.rds") %>% 
+  mutate(parent_metric = "Total Water Supply")
+
+napa_s_with_subcategoreies <- read_rds("data/napa_s_with_subcategories.rds") %>% 
+  mutate(parent_metric = "Total Water Supply")
+
+moulton_niguel_s_with_subcategoreies <- read_rds("data/moulton_niguel_s_with_subcategories.rds") %>% 
+  mutate(parent_metric = "Total Water Supply")
+
+supply_and_demand_volume_af_with_subcategories <- bind_rows(santa_fe_d_with_subcategories, napa_d_with_subcategoreies, moulton_niguel_d_with_subcategoreies, 
+                                                            santa_fe_s_with_subcategories, napa_s_with_subcategoreies, moulton_niguel_s_with_subcategoreies) %>% glimpse()
 
 
 write_rds(supply_and_demand_volume_af_with_subcategories, "data/supply_and_demand_volume_af_with_subcategories.rds")
